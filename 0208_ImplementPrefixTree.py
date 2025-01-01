908ba5804fcb85d23d314ff8fed294d46acb2917
edmarelie <eddarmawan@gmail.com>
2025-01-02 07:54:39 +0800

    Create 0208_ImplementPrefixTree.py

---
+++ 0208_ImplementPrefixTree.py
@@ -0,0 +1,49 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+
+        # For each character, create a children of another TrieNode
+        # For last character, set the endOfWord to True
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+
+        # Search each character in the Prefix Tree down to the children
+        # For last character, the endOfWord must be True
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.endOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+
+        # Search each character in the Prefix Tree down to the children
+        # For last character, the endOfWord doesn't have to be True
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
