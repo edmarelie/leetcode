0480b13ece0c598cf68e294087edd953603d1ccf
edmarelie <eddarmawan@gmail.com>
2025-07-20 22:03:05 +0800

    update 119 java

--- java/0119_PascalTriangleII.java
+++ java/0119_PascalTriangleII.java
@@ -1,33 +1,17 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<List<Integer>> triangle = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
 
-        List<Integer> firstRow = new ArrayList<>();
-        firstRow.add(1);
-        triangle.add(firstRow);
+        // Base case
+        row.add(1);
 
-        if (rowIndex==0) {
-            return triangle.get(rowIndex);
-        }
-
+        // Iterate from first row up to the target rowIndex
         for (int i=1; i<=rowIndex; i++) {
-            List<Integer> prevRow = triangle.get(i-1);
-            List<Integer> curRow = new ArrayList<>();
-
-            // 1st element
-            curRow.add(1);
-
-            // Middle element
-            for (int j=1; j<i; j++) {
-                curRow.add(prevRow.get(j-1) + prevRow.get(j));
+            for (int j=i-1; j>0; j--) {
+                row.set(j, row.get(j) + row.get(j-1));
             }
-
-            // Last element
-            curRow.add(1);
-
-            // Update triangle
-            triangle.add(curRow);
+            row.add(1);
         }
-        return triangle.get(rowIndex);
+        return row;
     }
 }
