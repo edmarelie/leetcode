3bccd0c77fa7e533fff5f0ec378f9b8f2100783d
edmarelie <eddarmawan@gmail.com>
2025-07-20 19:20:13 +0800

    update 0271 java

--- java/0271_EncodeAndDecode.java
+++ java/0271_EncodeAndDecode.java
@@ -2,27 +2,34 @@ class Solution {
 
     public String encode(List<String> strs) {
         StringBuilder res = new StringBuilder();
-        for (String s : strs) {
-            res.append(s.length()).append('#').append(s);
+        for (String str : strs) {
+            res.append(str.length()).append("#").append(str);
         }
-        System.out.println(res.toString());
         return res.toString();
     }
 
     public List<String> decode(String str) {
-        //          i
-        //              j
-        // str = "4#neet4#code4#love3#you"
+        // Init res List
         List<String> res = new ArrayList<>();
+
         int i = 0;
+
+        // Loop through the string
         while (i < str.length()) {
             int j = i;
+            // Moves pointer j to '#'' to get the string length
+            // Use single quote for char
             while (str.charAt(j) != '#') {
                 j++;
             }
+
+            // Gets length
             int length = Integer.parseInt(str.substring(i, j));
+            // Moves pointer i to first letter
             i = j + 1;
+            // Moves pointer j to last letter + 1
             j = i + length;
+            // Stores substring to res.
             res.add(str.substring(i, j));
             i = j;
         }
