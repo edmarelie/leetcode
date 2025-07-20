ff1f3bf7cb4a03389fbe9ebc151055d64ae0f83e
edmarelie <eddarmawan@gmail.com>
2025-07-20 17:57:37 +0800

    add 0347 java

---
+++ java/0347_TopKFrequentElements.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        // Create HashMap with occurrent of each element
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0)+1);
+        }
+
+        // Use MinHeap to sort most occurrence
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        // Iterate through HashMap
+        for (int num : count.keySet()) {
+            heap.offer(new int[]{count.get(num), num});
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        // Populate the result
+        int [] res = new int[k];
+        for (int i=0; i<k; i++) {
+            res[i] = heap.poll()[1];
+        }
+
+        return res;
+    }
+}
